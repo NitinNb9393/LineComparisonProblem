5f676eaa2e3cc80e8351607930805cd302f05665
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-20 12:50:43 +0530

    Added:CheckEquality

--- src/com/bridhelabz/linecomparison/LineComparison.java
+++ src/com/bridhelabz/linecomparison/LineComparison.java
@@ -8,6 +8,7 @@ public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison");
 		LengthCalculate();
+		toCheckEquality();
 	}
 
 	public static void LengthCalculate() {
@@ -19,8 +20,40 @@ public class LineComparison {
 		int y1 = scanner.nextInt();
 		System.out.println("Enter the y2 co-ordinate");
 		int y2 = scanner.nextInt();
-
-		double length = Math.sqrt((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)));
-		System.out.println("The length of line is " + length);
 	}
+
+	public static void toCheckEquality() {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter x co-ordinate of first point: ");
+		int x1 = s.nextInt();
+		System.out.println("Enter y co-ordinate of first point: ");
+		int y1 = s.nextInt();
+		System.out.println("Enter x co-ordinate of second point: ");
+		int x2 = s.nextInt();
+		System.out.println("Enter y co-ordinate of second point: ");
+		int y2 = s.nextInt();
+
+		System.out.println("Enter x co-ordinate of third point: ");
+		int p1 = s.nextInt();
+		System.out.println("Enter y co-ordinate of third point: ");
+		int q1 = s.nextInt();
+		System.out.println("Enter x co-ordinate of forth point: ");
+		int p2 = s.nextInt();
+		System.out.println("Enter y co-ordinate of forth point: ");
+		int q2 = s.nextInt();
+		s.close();
+
+		Double length_of_line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		Double length_of_line2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
+
+		System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
+		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
+		boolean ans = length_of_line1.equals(length_of_line2);
+		if (ans)
+			System.out.println("Both lines are equal.");
+		else
+			System.out.println("Both lines are not equal.");
+	}
+
 }
